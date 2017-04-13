d7e023c42fbf2eb2dc263958a539154d325aee98
sojourned <sjones66@msn.com>
2017-04-13 10:05:11 -0700

    Initial commit

---
+++ RightAngleSegment/RightAngleSegment/RightAngleCom.cs
@@ -0,0 +1,150 @@
+﻿using ESRI.ArcGIS.ADF.BaseClasses;
+using ESRI.ArcGIS.ADF.CATIDs;
+using ESRI.ArcGIS.ArcMapUI;
+using ESRI.ArcGIS.Editor;
+using ESRI.ArcGIS.Framework;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.SystemUI;
+using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+
+
+namespace RightAngleSegment
+{
+  [Guid("e7b03140-8f37-11dd-ad8b-0800200c9a66")]
+  [ClassInterface(ClassInterfaceType.None)]
+  [ProgId("RightAngleSegment.RightAngleCom")]
+
+  public sealed class RightAngleCom : BaseCommand
+  {
+    #region COM Registration Function(s)
+    [ComRegisterFunction()]
+    [ComVisible(false)]
+    static void RegisterFunction(Type registerType)
+    {
+      // Required for ArcGIS Component Category Registrar support
+      ArcGISCategoryRegistration(registerType);
+
+      //
+      // TODO: Add any COM registration code here
+      //
+    }
+
+    [ComUnregisterFunction()]
+    [ComVisible(false)]
+    static void UnregisterFunction(Type registerType)
+    {
+      // Required for ArcGIS Component Category Registrar support
+      ArcGISCategoryUnregistration(registerType);
+
+      //
+      // TODO: Add any COM unregistration code here
+      //
+    }
+
+    #region ArcGIS Component Category Registrar generated code
+    /// <summary>
+    /// Required method for ArcGIS Component Category registration -
+    /// Do not modify the contents of this method with the code editor.
+    /// </summary>
+    private static void ArcGISCategoryRegistration(Type registerType)
+    {
+      string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+      MxCommands.Register(regKey);
+
+    }
+    /// <summary>
+    /// Required method for ArcGIS Component Category unregistration -
+    /// Do not modify the contents of this method with the code editor.
+    /// </summary>
+    private static void ArcGISCategoryUnregistration(Type registerType)
+    {
+      string regKey = string.Format("HKEY_CLASSES_ROOT\\CLSID\\{{{0}}}", registerType.GUID);
+      MxCommands.Unregister(regKey);
+
+    }
+
+    #endregion
+    #endregion
+
+    private IEditor m_editor;
+    private IEditSketch3 m_edSketch;
+    private IApplication m_application;
+
+    public RightAngleCom()
+    {
+      base.m_category = "Developer Samples"; //localizable text
+      base.m_caption = "Right Angle Segment Constructor"; //localizable text
+      base.m_message = "Creates right angle segments"; //localizable text 
+      base.m_toolTip = "Right Angle Segment"; //localizable text 
+      base.m_name = "DeveloperSamples_RightAngleSegment"; //unique id, non-localizable (e.g. "MyCategory_ArcMapCommand")
+      base.m_bitmap = new Bitmap(GetType().Assembly.GetManifestResourceStream("RightAngleSegment.RightAngle.bmp"));
+    }
+
+    #region Overriden Class Methods
+    public override void OnCreate(object hook)
+    {
+      if (hook == null)
+        return;
+
+      m_application = hook as IApplication;
+
+      //Disable if it is not ArcMap
+      if (hook is IMxApplication)
+        base.m_enabled = true;
+      else
+        base.m_enabled = false;
+
+      //get the editor
+      UID editorUid = new UID();
+      editorUid.Value = "esriEditor.Editor";
+      m_editor = m_application.FindExtensionByCLSID(editorUid) as IEditor;
+      m_edSketch = m_editor as IEditSketch3;
+    }
+
+    /// <summary>
+    /// Occurs when this command is clicked
+    /// </summary>
+    public override void OnClick()
+    {
+      //m_edSketch = m_editor as IEditSketch3;
+      RightAngleSC prasc = new RightAngleSC();
+      prasc.Initialize(m_editor);
+      m_edSketch.ShapeConstructor = prasc;
+    }
+    
+    public override bool Enabled
+    {
+      // Enable the command if editing 
+      // and current tool implements IShapeContructorTool
+      get 
+      { 
+        bool pEnabled = false;
+        if (m_editor.EditState == esriEditState.esriStateEditing)
+        {
+          //Check for IShapeConstructorTool
+          IShapeConstructorTool psct = m_application.CurrentTool.Command as IShapeConstructorTool;
+          pEnabled = (psct != null);
+        }
+        return (pEnabled); 
+      }
+    }
+
+    public override bool Checked
+    {
+      get
+      {
+        // Check the command/button if we are the current constructor
+        IPersist ptemp = m_edSketch.ShapeConstructor as IPersist;
+        Guid pg;
+        ptemp.GetClassID(out pg);
+        return (pg.ToString() == "8bfa7de0-8f39-11dd-ad8b-0800200c9a66");
+      }
+    }
+
+    #endregion
+
+  }
+}
---
+++ RightAngleSegment/RightAngleSegment/RightAngleSC.cs
@@ -0,0 +1,201 @@
+﻿using ESRI.ArcGIS.Editor;
+using ESRI.ArcGIS.esriSystem;
+using ESRI.ArcGIS.Geometry;
+using System;
+
+namespace RightAngleSegment
+{
+
+  class RightAngleSC : StraightConstructorClass, IPersist
+  {
+    private IEditor m_editor;
+    private IEditSketch3 m_EdSketch;
+
+    public override void Activate()
+    {
+      base.Activate();
+    }
+
+    public override bool Active
+    {
+      get
+      {
+        return base.Active;
+      }
+    }
+    
+    public override ESRI.ArcGIS.Geometry.IPoint Anchor
+    {
+      get
+      {
+        return base.Anchor;
+      }
+    }
+
+    public override double AngleConstraint
+    {
+      get
+      {
+        return base.AngleConstraint;
+      }
+      set
+      {
+        base.AngleConstraint = value;
+      }
+    }
+
+    public override esriSketchConstraint Constraint
+    {
+      get
+      {
+        return base.Constraint;
+      }
+      set
+      {
+        base.Constraint = value;
+      }
+    }
+
+
+    public override void AddPoint(ESRI.ArcGIS.Geometry.IPoint point, bool Clone, bool allowUndo)
+    {
+      base.AddPoint(point, Clone, allowUndo);
+    }
+
+    public override void Initialize(IEditor pEditor)
+    {
+      m_editor = pEditor;
+      m_EdSketch = m_editor as IEditSketch3;
+      base.Initialize(pEditor);
+    }
+
+    public override int Cursor
+    {
+      get
+      {
+        return base.Cursor;
+      }
+    }
+    public override void Deactivate()
+    {
+      base.Deactivate();
+    }
+    public override double DistanceConstraint
+    {
+      get
+      {
+        return base.DistanceConstraint;
+      }
+      set
+      {
+        base.DistanceConstraint = value;
+      }
+    }
+    public override bool Enabled
+    {
+      get
+      {
+        return base.Enabled;
+      }
+    }
+    public override bool Equals(object obj)
+    {
+      return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+      return base.GetHashCode();
+    }
+    public override string ID
+    {
+      get
+      {
+        return base.ID;
+      }
+    }
+    public override bool IsStreaming
+    {
+      get
+      {
+        return base.IsStreaming;
+      }
+      set
+      {
+        base.IsStreaming = value;
+      }
+    }
+    public override ESRI.ArcGIS.Geometry.IPoint Location
+    {
+      get
+      {
+        return base.Location;
+      }
+    }
+    public override bool OnContextMenu(int X, int Y)
+    {
+      return base.OnContextMenu(X, Y);
+    }
+    public override void OnKeyDown(int keyState, int shift)
+    {
+      base.OnKeyDown(keyState, shift);
+    }
+    public override void OnKeyUp(int keyState, int shift)
+    {
+      base.OnKeyUp(keyState, shift);
+    }
+    public override void OnMouseDown(int Button, int shift, int X, int Y)
+    {
+      base.OnMouseDown(Button, shift, X, Y);
+    }
+    public override void OnMouseMove(int Button, int shift, int X, int Y)
+    {
+      //Determine tangent to last segment on edit sketch
+      //Set as angle constraint +90
+
+      ISegmentCollection pSc = m_EdSketch.Geometry as ISegmentCollection;
+      if (pSc.SegmentCount > 0)
+      {
+        ICurve3 pCurve;
+        if (m_EdSketch.GeometryType == esriGeometryType.esriGeometryPolygon)
+          pCurve = pSc.get_Segment(pSc.SegmentCount - 2) as ICurve3;
+        else
+          pCurve = pSc.get_Segment(pSc.SegmentCount - 1) as ICurve3;
+
+        ILine pLine = new Line();
+        pCurve.QueryTangent(esriSegmentExtension.esriExtendTangentAtTo, 1, true, 10, pLine);
+
+        base.AngleConstraint = pLine.Angle + (90 * Math.PI / 180);
+        base.Constraint = esriSketchConstraint.esriConstraintAngle;
+      }
+      base.OnMouseMove(Button, shift, X, Y);
+    }
+    public override void OnMouseUp(int Button, int shift, int X, int Y)
+    {
+      base.OnMouseUp(Button, shift, X, Y);
+    }
+    public override void Refresh(int hdc)
+    {
+      base.Refresh(hdc);
+    }
+    public override void SketchModified()
+    {
+      base.SketchModified();
+    }
+    public override string ToString()
+    {
+      return base.ToString();
+    }
+
+    #region IPersist Members
+
+    public void GetClassID(out Guid pClassID)
+    {
+      //Explicitly set a guid so its different from the aggregated straight constructor
+      //Used to set command.checked property
+      pClassID = new Guid("8bfa7de0-8f39-11dd-ad8b-0800200c9a66");
+    }
+
+    #endregion
+
+  }
+}
